d6a620af9cb3258aac60c02e9284bfeb2aae0ee6
georgewhewell <george.whewell@artsalliancemedia.com>
2016-04-08 11:21:32 +0000

    adds lock around stat dumping for use in threaded applications

--- profilestats.py
+++ profilestats.py
@@ -1,11 +1,11 @@
 from cProfile import Profile
 from functools import wraps
 import pstats
-
+import threading
 import pyprof2calltree
 
 profiler = Profile()
-
+lock = threading.Lock()
 
 def profile(cumulative=True, print_stats=0, sort_stats='cumulative',
             dump_stats=False, profile_filename='profilestats.out',
@@ -21,15 +21,16 @@ def profile(cumulative=True, print_stats=0, sort_stats='cumulative',
             try:
                 result = profiler.runcall(func, *args, **kwargs)
             finally:
-                if dump_stats:
-                    profiler.dump_stats(profile_filename)
-                stats = pstats.Stats(profiler)
-                conv = pyprof2calltree.CalltreeConverter(stats)
-                with open(callgrind_filename, 'w') as fd:
-                    conv.output(fd)
-                if print_stats:
-                    stats.strip_dirs().sort_stats(
-                        sort_stats).print_stats(print_stats)
+                with lock:
+                    if dump_stats:
+                        profiler.dump_stats(profile_filename)
+                    stats = pstats.Stats(profiler)
+                    conv = pyprof2calltree.CalltreeConverter(stats)
+                    with open(callgrind_filename, 'w') as fd:
+                        conv.output(fd)
+                    if print_stats:
+                        stats.strip_dirs().sort_stats(
+                            sort_stats).print_stats(print_stats)
             return result
         return decorator
     return closure
